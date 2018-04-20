ae186359d9daafb31d968eaa2a98f2679720c301
JayAv8789 <avinashc@tv18.com>
2018-04-20 15:38:34 +0530

    node prototype

---
+++ node_api/config/dbconnection.js
@@ -0,0 +1,17 @@
+var configValues = require('./config');
+
+var mySql = require('mysql');
+
+var dbConnection = mySql.createConnection({
+    user    :   configValues.user,
+    host    :   configValues.host,
+    password:   configValues.password,
+    database:   configValues.database
+});
+
+var connection = dbConnection.connect(function(err){
+    if(err) throw err;
+    console.log('Database connection is established');
+})
+
+module.exports = dbConnection;
