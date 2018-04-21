8c56bc31f20ef11e53f53c632d792585d30e8a0d
JayAv8791 <avinash.c.pal@gmail.com>
2018-04-21 12:00:27 +0530

    00

--- node_api/index.js
+++ node_api/index.js
@@ -10,22 +10,4 @@ node_api.use(bodyParser.urlencoded({extended:true}));
 node_api.use(bodyParser.json());
 
 node_api.post('/api/registerUser', registerController.registerUser);
-node_api.listen(5500);
-
-
-
-
-
-
-
-// // var authenticateController=require('./controllers/authenticate-controller');
-
-// app.use(bodyParser.urlencoded({extended:true}));
-
-// app.use(bodyParser.json());
-
-// /* route to handle login and registration */
-// app.post('/api/test',registerController.register);
-// app.listen(8000);
-
-// // app.post('/api/authenticate',authenticateController.authenticate);
+node_api.listen(1100);
